29b8e499915b9c274891ddf49355f83d184199f5
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-28 02:45:57 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,8 +1,9 @@
 def my_find(collection)
-i = 0
-while i < collection.length
-return collection[i]
-if yield(collection[i])
+  i = 0
+  while i < collection.length
+  return collection[i]
+  if 
+  yield(collection[i])
   i = i + 1
 end
 end
